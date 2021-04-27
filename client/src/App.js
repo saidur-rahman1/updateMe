ddfd7fa13d6628bf9bf0de1b7aec89249ae90b6c
Saidur Rahman <47373074+saidur-rahman1@users.noreply.github.com>
2021-04-27 02:43:27 -0400

    Setup database and sign in page
    - MongoDB was chosen
    - In order to run the project, first cmd was open as an admin and mongo was run by simply typing, mongod
    - Keeping this command prompt open, a new cmd window was opened as an admin and the server was run by going into the server folder and typing, npm run dev. On doing so, it would show that its connected to the db if it could form a successful connection
    - Keeping both these windows open, a third cmd window was opened as an admin. This time, went into the client folder and typed, npm start, to open the page
    - The page is a simple page with no connection to the backend as of yet. Not sure if is what was needed or a connection with the backend was also required in this issue.

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,46 @@ import "./App.css";
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path="/" component={LandingPage} />
-      </BrowserRouter>
-    </MuiThemeProvider>
+    <div class="page-login">
+      <div class="ui centered grid container">
+        <div class="nine wide column">
+          <div class="header">
+            <h3><center>Sign in to your account</center></h3>
+          </div>
+          {/* <div class="ui icon warning message">
+              <i class="lock icon"></i>
+              <div class="content">
+                <div class="header">
+                  Login failed!
+                </div>
+                <p>You might have misspelled your username or password!</p>
+              </div>
+          </div> */}
+          <div class="ui fluid card">
+            <div class="content">
+            <form class="ui form" method="POST">
+              <div class="field">
+                <input type="text" name="username" placeholder="Username" />
+              </div>
+              <div class="field">
+                <input type="password" name="password" placeholder="Password" />
+              </div>
+              <button class="ui primary labeled icon button" type="submit">
+                <i class="unlock alternate icon"></i>
+                Login
+              </button>
+            </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    // { <MuiThemeProvider theme={theme}>
+    //   <BrowserRouter>
+    //     <Route path="/" component={LandingPage} />
+    //   </BrowserRouter>
+    // </MuiThemeProvider> }
   );
 }
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/updateMe')
+.then(() => console.log("Connected to MongoDB"))
+.catch(error => console.error("Could not connect to MongoDB", error));
 
 router.get("/welcome", function (req, res, next) {
   res.status(200).send({ welcomeMessage: "Step 1 (completed)" });
