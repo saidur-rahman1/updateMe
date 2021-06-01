577518b556ba6cdb1f2b26e8a6c7686bb881c44d
Saidur Rahman <47373074+saidur-rahman1@users.noreply.github.com>
2021-06-01 02:27:53 -0400

    update3

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const dotenv = require("dotenv");
 
-dotenv.config();
-
-mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`)
-.then(() => console.log("Connected to MongoDB"))
-.catch(error => console.error("Could not connect to MongoDB", error));
 
 router.get("/welcome", function (req, res, next) {
   res.status(200).send({ welcomeMessage: "Step 1 (completed)" });
