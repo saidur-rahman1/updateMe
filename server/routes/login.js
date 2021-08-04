09747e28448622391a69a896a6d35b93bbbcc29c
Saidur Rahman <47373074+saidur-rahman1@users.noreply.github.com>
2021-08-04 05:22:37 -0400

    submission4

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,25 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const User = require("../models/user");
 
-router.post("/login", function(req, res, next) {
+router.post("/login", async (req, res) => {
 
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  async function checkUser() {
-    try {
-      const user = await User.find({email: email, password: password});
-      if (!user) return res.status(400).send("Invalid credentials/User not found");
-      res.status(201).send("Login successful");
-    } catch (error) {
-      console.log(error);
+    if (!email || !password) {
+      return res.status(400).send("All required fields not complete");
     }
-  
+
+    const user = await User.findOne({email: email});
+    if (!user) return res.status(401).send("Invalid credentials/User not found");
+
+    const passwordCheck = await bcrypt.compare(password, user.password);
+    if (!passwordCheck) return res.status(401).send("Invalid credentials/User not found");
+
+    const token = jwt.sign(
+      {
+        user: user._id
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true
+    }).send();
+
+    res.status(201).send("Login successful");
+
+  } catch (error) {
+    console.log(error);
   }
-
-  checkUser();
-
+  
 });
 
 module.exports = router;
--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,50 +1,45 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const mongoose = require('mongoose');
 
 const User = require("../models/user");
 
 router.post("/signup", async (req, res) => {
-    const { firstName, lastName, email, password1, password2 } = req.body;
-    let errorCheck = false;
 
-    if (!firstName && !lastName && !email && !password1 && !password2) {
-      errorCheck = true;
+  try {
+    
+    const { firstName, lastName, email, password1, password2 } = req.body;
+
+    if (!firstName || !lastName || !email || !password1 || !password2) {
       return res.status(400).send("All required fields not complete");
     }
     
     let isValidEmail = (/$^|.+@.+..+/).test(values.email);
-    if (!isValidEmail) {
-      errorCheck = true;
-      return res.status(400).send("Invalid email");
-    }
+    if (!isValidEmail) return res.status(400).send("Invalid email");
 
-    if (password1.length < 6) {
-      errorCheck = true;
-      return res.status(400).send("Password should be at least 6 characters");
-    }
+    if (password1.length < 6) return res.status(400).send("Password should be at least 6 characters");
+
+    if (password1 !== password2) return res.status(400).send("Passwords do not match");
 
     let user = await User.findOne({email: email});
-    if (user) {
-      errorCheck = true;
-      return res.status(400).send("Account already registered");
-    }
+    if (user) return res.status(400).send("Account already registered");
 
-    if (!errorCheck) {
-      user = new User({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password1
-      });
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(user.password, salt);
-      const result = await user.save();
-      console.log(result);
-      res.send(user);
-      res.status(201).send("Registration successful");
-    }
+    user = new User({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password1
+    });
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password, salt);
+    const result = await user.save();
+    console.log(result);
+    res.status(201).send(user);
+
+  } catch (error) {
+    console.log(error);
+  }
+    
 });
 
 module.exports = router;
