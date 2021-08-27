050fc2c2a3bc5d2227dbeae77fc6f217f4ed6c20
Saidur Rahman <47373074+saidur-rahman1@users.noreply.github.com>
2021-08-27 15:03:59 -0400

    fe: mention ui #15 - 2
    Updates:
    - Modified Dashboard.js and Mention.js so that Modified can accept props from Dashboard.

--- client/src/components/Mention.js
+++ client/src/components/Mention.js
@@ -4,7 +4,6 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
-import reddit from '../icons/reddit.png';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,7 +29,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function Mention() {
+export default function Mention(props) {
   const classes = useStyles();
 
   return (
@@ -38,25 +37,25 @@ export default function Mention() {
         <Grid container spacing={2}>
             <Grid item>
                 <ButtonBase className={classes.image}>
-                    <img className={classes.img} alt="reddit" src={reddit} />
+                    <img className={classes.img} alt={props.alt} src={props.imgSource} />
                 </ButtonBase>
             </Grid>
             <Grid item xs={12} sm container>
                 <Grid item container>
                     <Typography variant="h6">
-                        Microsoft pays US 8 billion to acquire&nbsp;
+                        {props.title1}&nbsp;
                     </Typography>
                     <Typography variant="h6" color="primary">
-                        Github
+                        {props.title2}
                     </Typography>
                 </Grid>
                 <Grid item xs container direction="column" spacing={2}>
                     <Grid item xs>
                         <Typography variant="body2" gutterBottom color="textSecondary">
-                            Business Insider
+                            {props.contentSource}
                         </Typography>
                         <Typography variant="body2" color="textSecondary">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            {props.text}
                         </Typography>
                     </Grid>
                 </Grid>
