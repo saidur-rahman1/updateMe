dbf23f7d05360c9418b9b72e0ef1eb399f9b9007
Saidur Rahman <47373074+saidur-rahman1@users.noreply.github.com>
2021-10-12 23:42:33 -0400

    update ui and resolve resolve mention fetching bug 4
    Moved the Styles.js file from the components folder to src and renamed it to styles.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { useStyles } from './Styles.js';
+import { useStyles } from '../styles.js';
 import SettingsIcon from '@material-ui/icons/Settings';
 
 export default function NavBar() {
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { useStyles } from '../components/Styles.js';
+import { useStyles } from '../styles.js';
 import axios from 'axios';
 
 const initialValues = {
