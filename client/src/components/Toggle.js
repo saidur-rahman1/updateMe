d3e06c58ce55ed2da4b00032c0146c31b419127e
Saidur Rahman <47373074+saidur-rahman1@users.noreply.github.com>
2022-01-13 00:39:28 -0500

    fe reordering mentions #1 2
    Updates:
    - Added toggle buttons and their functionalities
    
    Issues:
    - Buttons seem to operate the other way around i.e. popular button gives recent data and recent button gives popular data. Also when the page is first loaded or refreshed, recent button is selected but recent data is not sorted out and displayed

--- client/src/components/Toggle.js
+++ client/src/components/Toggle.js
@@ -2,55 +2,63 @@ import * as React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper } from '@material-ui/core';
 import { ToggleButtonGroup, ToggleButton } from '@material-ui/lab'
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
+  togglePaper: {
     marginTop: theme.spacing(0),
     alignItems: 'center',
-    background: '#eceffd',
-    padding: '0.5rem',
+    background: '#ccd2e6',
+    padding: '0.2rem',
     borderRadius: '35px',
   },
-  tab: {
+  toggle: {
     cursor: 'pointer',
-    color: 'primary',
-    borderColor: 'white',
+    color: theme.palette.primary.main,
+    borderColor: '#ccd2e6',
     borderRadius: '35px',
     padding: '0.5rem',
     "&.Mui-selected": {
-      background: theme.palette.primary.main
+      background: theme.palette.primary.main,
+      color: 'white'
     },
+    textTransform: 'none',
+    width: '9rem',
+    height: '2rem'
   },
-  tabs: {
-    height: '1.5rem'
-  }
 }));
 
 export default function ToggleButtonNotEmpty() {
   const classes = useStyles();
-  const [alignment, setAlignment] = React.useState('left');
+  const [order, setOrder] = React.useState('recent');
 
-  const handleAlignment = (event, newAlignment) => {
-    if (newAlignment !== null) {
-      setAlignment(newAlignment);
+  const handleOrder = (event, newOrder) => {
+    if (newOrder !== null) {
+      setOrder(newOrder);
     }
   };
 
   return (
+    <Grid item container className={classes.togglePaper}>
       <ToggleButtonGroup
-        value={alignment}
+        value={order}
         exclusive
-        onChange={handleAlignment}
-        aria-label="text alignment"
+        onChange={handleOrder}
       >
-        <ToggleButton value="recent">
+        <ToggleButton value="recent" className={classes.toggle}>
           <Typography>Most recent</Typography>
         </ToggleButton>
-        <ToggleButton value="popular">
+      </ToggleButtonGroup>
+      <ToggleButtonGroup
+        value={order}
+        exclusive
+        onChange={handleOrder}
+      >
+        <ToggleButton value="popular" className={classes.toggle}>
           <Typography>Most popular</Typography>
         </ToggleButton>
       </ToggleButtonGroup>
+    </Grid>
+      
   );
 }
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -8,7 +8,8 @@ import Mention from '../components/Mention';
 import CustomizedDialog from '../components/Dialog';
 import axios from 'axios';
 import { useParams, useHistory } from "react-router-dom";
-import Toggle from '../components/Toggle';
+import { ToggleButtonGroup, ToggleButton } from '@material-ui/lab'
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,7 +26,28 @@ const useStyles = makeStyles((theme) => ({
   },
   top: {
     marginBottom: theme.spacing(3)
-  }
+  },
+  togglePaper: {
+    marginTop: theme.spacing(0),
+    alignItems: 'center',
+    background: '#ccd2e6',
+    padding: '0.2rem',
+    borderRadius: '35px',
+  },
+  toggle: {
+    cursor: 'pointer',
+    color: theme.palette.primary.main,
+    borderColor: '#ccd2e6',
+    borderRadius: '35px',
+    padding: '0.5rem',
+    "&.Mui-selected": {
+      background: theme.palette.primary.main,
+      color: 'white'
+    },
+    textTransform: 'none',
+    width: '9rem',
+    height: '2rem'
+  },
 }));
 
 export default function Dashboard() {
@@ -35,6 +57,9 @@ export default function Dashboard() {
 
   const [open, setOpen] = useState(false);
   const [mention, setMention] = useState(null);
+  const [order, setOrder] = useState("recent");
+
+  
 
   const [mentions, setMentions] = useState([]);
   useEffect(() => {
@@ -43,6 +68,22 @@ export default function Dashboard() {
     .then(res => setMentions(res.data))
     .catch(error => console.log(error));
   }, []);
+
+  useEffect(() => {
+    let orderedMentions = [];
+    if (order === 'recent') {
+      orderedMentions = mentions.sort((a,b) => a.date < b.date);
+      setMentions(orderedMentions);
+      console.log("Recent =>");
+      console.log(orderedMentions);
+    } else 
+    if (order === 'popular') {
+      orderedMentions = mentions.sort((a,b) => b.popularity - a.popularity);
+      setMentions(orderedMentions);
+      console.log("Popularity =>");
+      console.log(orderedMentions);
+    }
+  }, [order, mentions]);
 
   useEffect(() => {
     const openMentionDialog = (mention) => {
@@ -64,6 +105,16 @@ export default function Dashboard() {
     history.push(`/dashboard/${mention._id}`);
   }
 
+  const handleOrder = (event, newOrder) => {
+    if (newOrder !== null) {
+      setOrder(newOrder);
+    }
+  };
+
+  const toggleClick = (value) => {
+    setOrder(value);
+  }
+
   return (
     <div className={classes.root}>
       <NavBar />
@@ -75,9 +126,28 @@ export default function Dashboard() {
               <Grid item>
                 My mentions
               </Grid>
-              <Grid item xs={5} />
+              <Grid item xs={6} />
               <Grid item>
-                {/* <Toggle /> */}
+                <Grid item container className={classes.togglePaper}>
+                  <ToggleButtonGroup
+                    value={order}
+                    exclusive
+                    onChange={handleOrder}
+                  >
+                    <ToggleButton value="recent" className={classes.toggle} onClick={() => { toggleClick("recent") }}>
+                      <Typography>Most recent</Typography>
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                  <ToggleButtonGroup
+                    value={order}
+                    exclusive
+                    onChange={handleOrder}
+                  >
+                    <ToggleButton value="popular" className={classes.toggle} onClick={() => { toggleClick("popular") }}>
+                      <Typography>Most popular</Typography>
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                </Grid>
               </Grid>
             </Grid>
             <CustomizedDialog open={open} close={() => setOpen(false)} mention={mention} />
